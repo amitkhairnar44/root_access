24dd1cb3a16a340ec5c84d0252125d428713946a
Amit Khairnar <amitkhairnar44@gmail.com>
2020-04-18 09:44:53 +0530

    Update

--- lib/root_access.dart
+++ lib/root_access.dart
@@ -5,6 +5,10 @@ import 'package:flutter/services.dart';
 class RootAccess {
   static const MethodChannel _channel = const MethodChannel('root_access');
 
+  /// Triggers app to get root access
+  ///
+  /// If device is not rooted then this will return `false`.
+  /// Else it will open installed superuser or similar app's pop up asking for Permission.
   static Future<bool> get rootAccess async {
     final bool access = await _channel.invokeMethod('isAccessGiven');
     return access;
