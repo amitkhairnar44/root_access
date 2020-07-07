f644f218004923dc4db3e4a742c2d99cc3ea9c8e
Amit Khairnar <amitkhairnar44@gmail.com>
2020-07-07 22:04:30 +0530

    Use new Android plugin API

--- android/src/main/java/com/amitkhairnar/rootaccess/RootAccessPlugin.java
+++ android/src/main/java/com/amitkhairnar/rootaccess/RootAccessPlugin.java
@@ -1,34 +1,52 @@
 package com.amitkhairnar.rootaccess;
 
+import androidx.annotation.NonNull;
+
 import com.stericson.RootTools.RootTools;
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** RootAccessPlugin */
-public class RootAccessPlugin implements MethodCallHandler {
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "root_access");
-    channel.setMethodCallHandler(new RootAccessPlugin());
-  }
+/**
+ * RootAccessPlugin
+ */
+public class RootAccessPlugin implements FlutterPlugin, MethodCallHandler {
+    /**
+     * Plugin registration.
+     */
 
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    switch (call.method) {
-      case "isAccessGiven":
-        result.success(isAccessGiven());
-        break;
-      default:
-        result.notImplemented();
-        break;
+    private MethodChannel channel;
+
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "root_access");
+        channel.setMethodCallHandler(new RootAccessPlugin());
     }
-  }
 
-  private boolean isAccessGiven(){
-    return RootTools.isAccessGiven();
-  }
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+      if (call.method.equals("isAccessGiven")) {
+        result.success(isAccessGiven());
+      } else {
+        result.notImplemented();
+      }
+    }
+
+    private boolean isAccessGiven() {
+        return RootTools.isAccessGiven();
+    }
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "root_access");
+        channel.setMethodCallHandler(this);
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+    }
 }
